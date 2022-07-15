6048123d8c0b379923d555ef82cdb9c673ee37aa
Ali Kadhim <ali20140022@gmail.com>
2022-07-15 22:56:55 +0300

    Add favicon & add media query to make the website responbe

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-// document.addEventListener("scroll", ()=> {
-//     document.querySelector(".top_nav").style.position = "static";
-// })
+let logInBtn = document.getElementById("log-in-btn");
+let topNav = document.querySelector(".top_nav");
+
+console.log(topNav);
+
+window.onload = function () {
+  changeLogIn();
+  window.onresize = function () {
+    changeLogIn();
+  };
+};
+window.onscroll = function () {
+  fixNav();
+};
+
+function changeLogIn() {
+  if (window.innerWidth <= 768) {
+    logInBtn.innerHTML = `<i class="uil uil-user"></i>`;
+    logInBtn.classList = "btn-sec";
+  } else {
+    logInBtn.innerHTML = `<i class="uil uil-user"></i> Login / Register`;
+    logInBtn.classList = "btn";
+  }
+}
+
+function fixNav() {
+  if (window.scrollY >= 10) {
+    topNav.classList.add("active");
+  } else {
+    topNav.classList.remove("active");
+  }
+}
